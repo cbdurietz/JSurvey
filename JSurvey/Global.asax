68f9c46859a3f4610d439c9614ce4fc53e2afdee
CB Du Rietz <cbdurietz@gmail.com>
2014-11-20 23:41:24 +0100

    Initial commit.

---
+++ JSurvey/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JSurvey.MvcApplication" Language="C#" %>
---
+++ JSurvey/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JSurvey.Startup))]
+namespace JSurvey
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
